0ec00561a00dd7e30298cadfa49c0b9616c8d9d2
Stephen Haney <stephen.haney@gmail.com>
2025-02-19 11:50:12 -0800

    Adding some OG tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,10 +13,34 @@ export default function Layout({ children }: React.PropsWithChildren) {
 }
 
 export const metadata: Metadata = {
-  title: 'Paper',
+  title: 'Turn your logo into liquid metal | Paper',
+  description: 'Liquid metal for your logo by paper.design',
   icons: {
     icon: process.env.NODE_ENV === 'production' ? '/favicon.ico' : '/favicon-dev.ico',
     apple: '/apple-touch-icon.png',
+  },
+  openGraph: {
+    url: 'https://liquid.paper.design',
+    type: 'website',
+    locale: 'en_US',
+    siteName: 'Liquid logo by Paper',
+    title: 'Turn your logo into liquid metal | Paper',
+    description: 'Liquid metal for your logo by paper.design',
+    images: [
+      {
+        url: 'https://liquid.paper.design/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Turn your logo into liquid metal | Paper',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Turn your logo into liquid metal | Paper',
+    description: 'Liquid metal for your logo by paper.design',
+    creator: '@paper',
+    images: ['https://liquid.paper.design/og-image.png'],
   },
 };
 
